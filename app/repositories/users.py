a238a83355e58bbfd8bc00a7a7c92382813156b6
Rokima <55d55r77@gmail.com>
2025-11-30 23:04:41 +0300

    update repository users

--- app/repositories/users.py
+++ app/repositories/users.py
@@ -1,8 +1,27 @@
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
 from app.models.users import UserModel
 from app.repositories.base import BaseRepository
-from app.schemes.users import SUserGet
+from app.schemes.users import SUserGet, SUserGetWithRels
 
 
 class UsersRepository(BaseRepository):
     model = UserModel
     schema = SUserGet
+
+    async def get_one_or_none_with_role(self, **filter_by):
+        query = (
+            select(self.model)
+            .filter_by(**filter_by)
+            .options(selectinload(self.model.role))
+        )
+
+        result = await self.session.execute(query)
+
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
+        print(model.role.name)
+        result = SUserGetWithRels.model_validate(model, from_attributes=True)
+        return result
