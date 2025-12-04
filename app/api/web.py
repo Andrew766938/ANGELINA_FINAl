676429284a6f2a9d6cde2b6d42f616e303183213
Rokima <55d55r77@gmail.com>
2025-12-04 20:10:30 +0300

    add web router

--- app/api/web.py
+++ app/api/web.py
@@ -7,11 +7,11 @@ router = APIRouter(prefix="/pages", tags=["Фронтенд"])
 templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get('/reg')
+@router.get("/reg")
 async def get_registration_html(request: Request):
-    return templates.TemplateResponse(name='reg.html',
-                                      context={'request': request})
-@router.get('/login')
+    return templates.TemplateResponse(name="reg.html", context={"request": request})
+
+
+@router.get("/login")
 async def get_login_html(request: Request):
-    return templates.TemplateResponse(name='login.html',
-                                      context={'request': request})
+    return templates.TemplateResponse(name="login.html", context={"request": request})
