0e8bcdd253111807440e802a2245e266ee8576af
Andrew766938 <rnbrc122@gmail.com>
2025-12-16 03:33:04 +0300

    Add repositories __init__.py with exports

---
+++ app/repositories/__init__.py
@@ -0,0 +1,9 @@
+from app.repositories.flight_repository import FlightRepository, AirportRepository
+from app.repositories.booking_repository import BookingRepository, PaymentRepository
+
+__all__ = [
+    'FlightRepository',
+    'AirportRepository',
+    'BookingRepository',
+    'PaymentRepository',
+]
